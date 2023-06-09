1357837028623ad68fc65546ac8edafce80c6b06
Atish Dipankar Chakraborty <atishanindya@gmail.com>
2023-06-09 07:31:20 +0600

    get data from mongodb

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
@@ -25,7 +26,18 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const campDataCollection = client.db("campDb").collection("campData");
+
+    app.get('/campData', async (req, res) => {
+      const cursor = campDataCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
